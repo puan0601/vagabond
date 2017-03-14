68f4648166795b2976cbdc2d074106d371ed6ff7
Daniel Thompson <19dst89@gmail.com>
2017-03-14 16:08:53 -0700

    edit user page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -45,7 +45,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :city, :bio])
   # end
 
   # The path used after sign up.
