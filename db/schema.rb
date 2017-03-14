6ff4572ccb75c4343c159d41454e70150e14c6d3
Daniel Thompson <19dst89@gmail.com>
2017-03-14 12:08:18 -0700

    location model and controller with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314181642) do
+ActiveRecord::Schema.define(version: 20170314190252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "image"
+    t.string   "name"
+    t.string   "state"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
