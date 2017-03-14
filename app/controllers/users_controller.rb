0fa63fdd5c9ae776a77c33290c5ec46a968488c2
Daniel Thompson <19dst89@gmail.com>
2017-03-14 14:42:30 -0700

    users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
   devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  # User Routes
+  get '/users/:id', to: 'users#show', as: 'user'
   # Location Routes
   get '/cities', to: 'locations#index'
+
 end
