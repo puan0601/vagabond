07eb544e9059474eee53a2fef6044913c8f9a362
monzelb <monzelb@gmail.com>
2017-03-14 17:01:57 -0700

    added dropdown menu to new articles form. Not functional yet.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,4 +13,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.create(article_params)
+  end
+
+def article_params
+    params.require(:article).permit(:name)
+  end
 end
