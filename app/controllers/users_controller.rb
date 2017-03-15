aa06aac5886943eedd9e34dc31deb5b2f1b27877
Daniel Thompson <19dst89@gmail.com>
2017-03-14 21:43:07 -0700

    working redirect from edit page after a few mistakes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
-
-
-
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions'}
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
   # User Routes
   get '/users/:id', to: 'users#show', as: 'user'
+  as :user do
+    get 'users', :to => 'users#show', as: "user_root"
+  end
+
   # Location Routes
   get '/cities', to: 'locations#index'
   get '/cities/:id', to: 'locations#show', as: 'city'
@@ -12,6 +18,6 @@ Rails.application.routes.draw do
 
   # Articles Routes
   get '/articles', to: 'articles#index'
-  
+
 
 end
