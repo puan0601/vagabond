b88a5bf5f2d19408e7ea6e5b45c4b91dcad0ea38
medinal <loganjmedina@gmail.com>
2017-03-15 13:27:31 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,9 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, notice: "You don't have access to that page."
     end
   end
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 
 end
