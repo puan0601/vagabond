fff8b26c0caafc7bc16fa25706dfefa2c7701ba9
Logan Medina <loganjmedina@gmail.com>
2017-03-15 17:11:54 -0700

    fixed seed file some more

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,31 +12,26 @@ Article.destroy_all
 location_data = [
           { name: "Gibraltar",
             state: "British Overseas Territory",
-
             image: "http://vipweddingsspain.com/wp-content/uploads/2016/02/Gibraltar.jpg",
             country: "United Kingdom"
           },
           { name: "San Francisco",
             state: "California",
-
             image: "http://www.sftravel.com/sites/sftraveldev.prod.acquia-sites.com/files/SanFrancisco_0.jpg",
             country: "USA"
           },
           { name: "London",
             state: "Greater London",
-
             image: "http://cdn.pcwallart.com/images/london-at-night-hd-wallpaper-1.jpg",
             country: "United Kingdom"
           },
           { name: "San Jose",
             state: "California",
-
             image: "http://jamesmorrishomes.com/wp-content/uploads/2015/05/san_jose_downtown.jpg",
             country: "USA"
           },
           { name: "Las Vegas",
             state: "Nevada",
-
             image: "http://qtxasset.com/2016-12/las-vegas.jpg?YdgJOxAi7L2fpZtpby8TQORQi3wxsmrc",
             country: "USA"
           },
