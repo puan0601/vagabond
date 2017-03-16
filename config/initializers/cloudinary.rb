86ed77ee5dd3fc8fab038c776e654141c0495597
monzelb <monzelb@gmail.com>
2017-03-16 16:07:51 -0700

    Delete cloudinary.rb

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'babyhorsed'
-  config.api_key = '751433734171855'
-  config.api_secret = 'Qp1E9dC-w5exUR6XfXcynAPoyHA'
-  config.cdn_subdomain = true
-end
