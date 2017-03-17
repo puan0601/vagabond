d682430eebb9e455a3ac7ea58a062468b87162e3
Daniel Thompson <19dst89@gmail.com>
2017-03-17 14:21:05 -0700

    front page cards re-organized

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @locations = Location.order("RANDOM()").limit(1)
-    @articles = Article.order(id: :desc).limit(6)
+    @articles = Article.order(id: :desc).limit(3)
   end
 
 end
