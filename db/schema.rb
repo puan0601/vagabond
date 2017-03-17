f6919eea17d7e5a69923a3342a8861902175f117
monzelb <monzelb@gmail.com>
2017-03-16 17:27:11 -0700

    Added migrations for users age and locations about

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316211620) do
+ActiveRecord::Schema.define(version: 20170316235327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170316211620) do
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                                                                                                                                    null: false
     t.datetime "updated_at",                                                                                                                                    null: false
+    t.integer  "age"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
